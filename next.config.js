773af687a8c963ef79d348c997c5b731e1d3f4e4
github@finnesand.no <github@finnesand.no>
2020-03-07 16:54:49 +0100

    init

---
+++ next.config.js
@@ -0,0 +1,4 @@
+// const withPreact = require('@zeit/next-preact')
+// module.exports = withPreact({
+//   /* config options here */
+// })
---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import styles from './index.module.css'
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => {
+    import('@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js')
+    import('pimello')
+  },
+  {
+    ssr: false
+  }
+)
+
+const Home = () => {
+  // console.log('launch', launch())
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <DynamicComponentWithNoSSR />
+      <pimello-cms></pimello-cms>
+      <main className={styles.main}>
+        <h1 className='title'>
+          Welcome to <a href='https://nextjs.org'>Next.js!</a>
+        </h1>
+      </main>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Home
